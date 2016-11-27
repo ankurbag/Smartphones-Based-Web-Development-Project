5e688a53340c31bdf3ce83fd38074c98c1f55ac1
Sabrish Ramamoorthy <sramamoorthy@mobileaware.com>
2016-11-27 17:26:09 -0500

    adding connection dao

---
+++ SpringMVC/src/main/java/com/neu/mealpass/dao/ConnectionDao.java
@@ -0,0 +1,47 @@
+package com.neu.mealpass.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDao {
+	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/patient";// mysql://newton.neu.edu:3306/usersdb","student","p@sswOrd"
+	static final String USER = "root";
+	static final String PASS = "1234";
+	private static ConnectionDao connectionDao;
+
+	private ConnectionDao() {
+
+	}
+
+	public static ConnectionDao getConnectionDao() {
+		if (connectionDao == null)
+			connectionDao = new ConnectionDao();
+		return connectionDao;
+	}
+
+	public static Connection getConnection() {
+		Connection connection = null;
+		// STEP 2: Register JDBC driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// STEP 3: Open a connection
+		System.out.println("Connecting to database...");
+		try {
+			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Success!!!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+}
